542f3f257af96eddc58361c1ce844137ee9b6ef8
Kiki Rizki Arpiandi <kkrzkrk@gmail.com>
2017-10-11 16:38:39 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ -std=c++11 -Wl,-rpath='$ORIGIN/lib' -Iinclude -Llib label_image.cpp -ltensorflow_cc -o exec
+g++ -std=c++11 -Wl,-rpath='$ORIGIN/lib' -Iinclude -Llib main.cpp -ltensorflow_cc -o exec
 
 
